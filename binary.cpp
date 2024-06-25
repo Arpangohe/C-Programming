c72496ff1e7aaf533d6541beedb104623919d873
Arpan Gohe <166099844+Arpangohe@users.noreply.github.com>
2024-06-25 10:35:44 +0530

    Binary Operator Programm

---
+++ binary.cpp
@@ -0,0 +1,15 @@
+//Programm of Binary Operator
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1, num2;
+	cout<<"Enter Number 1 and Number 2:";
+	cin>>num1>>num2;  // cascadding of cin
+	//cin>>num1;
+	//cout<<"Enter Number 2:";
+	//cin>> num2;
+	cout<<"Addition :"<<num1+num2<<endl; //Cascading of cout
+	//cout<<num1+num2;
+	cout<<"Substraction:"<<num1-num2<<endl;
+}
