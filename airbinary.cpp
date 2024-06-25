e6fdc5ca6bd27716c5ad469531c843b2c997c266
Arpan Gohe <166099844+Arpangohe@users.noreply.github.com>
2024-06-25 10:47:58 +0530

    Arithmetic Binary Operator

---
+++ airbinary.cpp
@@ -0,0 +1,13 @@
+//Arithmetic binary
+#include<iostream>
+using namespace std;
+					
+
+int main(){
+	int a = 12;
+	int b = 5;
+	int c = a/b;
+	cout<<(float)a/b<<endl; // explicit
+	cout<<a/7.0<<endl;
+	cout<<'a'+1<<endl; //98 implicit
+}
