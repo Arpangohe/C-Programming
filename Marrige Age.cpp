c7b7aa2fde9a62f4ec4aef686d915c485e7d0bad
Arpan Gohe <166099844+Arpangohe@users.noreply.github.com>
2024-06-26 22:34:20 +0530

    Check Age is Eligible for Marriage

---
+++ Marrige Age.cpp
@@ -0,0 +1,17 @@
+//Create a program to check age is eligible for marriage or not
+#include<iostream>
+using namespace std;
+
+int main(){
+	int age;
+	char gender;
+	
+	cout<<"Enter your Age :"<<endl;
+	cin>>age;
+	
+	cout<<"Enter your Gender 'M' for Male 'F' for Female :"<<endl;
+	cin>>gender;
+	
+	if(age ==<21 && gender == male);
+
+}
