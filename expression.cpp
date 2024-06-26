771830b57bf10c78d8c9451d66f7663706a01d3d
Arpan Gohe <166099844+Arpangohe@users.noreply.github.com>
2024-06-26 09:35:08 +0530

    compression operator (Expression)

---
+++ expression.cpp
@@ -0,0 +1,12 @@
+// Expression -->airtmetic and logical a>b
+// compression operator
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a=90, b=67, c=90;
+	cout<<"a > b ----->"<<(a>b)<<endl;
+	cout<<"a >= b----->"<<(a>=b)<<endl;
+	cout<<"a==b ----->"<<(a==b)<<endl;
+	cout<<"a != b----->"<<(a!=b)<<endl;
+}
