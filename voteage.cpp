862c2f9da96ee3022683e7ce98f7a15beacfd1f8
Arpan Gohe <166099844+Arpangohe@users.noreply.github.com>
2024-06-27 22:47:15 +0530

    Checking Vote eligibility

---
+++ voteage.cpp
@@ -0,0 +1,13 @@
+//Write a program o read age of a candidate and determine whether 
+//he is eligible to cast his/her own vote using ternary operator
+#include<iostream>
+using namespace std;
+
+int main(){
+	int age;
+	
+	cout<<"Enter Your Age"<<endl;
+	cin>>age;
+	
+	age >=18 ? cout<<"You are Eligible"<<endl : cout<<"You Are Not Eligible"<<e
+}
