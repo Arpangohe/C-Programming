bdd4140f0fede0aa6e8efd01170b68ebd70519ae
Arpan Gohe <166099844+Arpangohe@users.noreply.github.com>
2024-07-01 16:50:10 +0530

    Accept Integer

---
+++ integer check.cpp
@@ -0,0 +1,14 @@
+//Accept integer and equal or not using turnary operator
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1, num2;
+	cout<<"Enter the first number"<<endl;
+	cin>>num1;
+	
+	cout<<"Enter the secound number"<<endl;
+	cin>>num2;
+	
+	num1 == num2 ? cout<<"The Numbers or Equal"<<endl : cout<<"The Numbers are not Equal"<<endl;
+}
