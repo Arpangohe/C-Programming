4dd77052609fbb4140562653a03356315633862c
arpangohe <arpangohe02@gmail.com>
2024-07-03 15:31:19 +0530

    Check White Space

---
+++ checkwhitespace.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str="apple is good for health";
+    int count=0;
+    for(int i=0;i<str.size();i++){
+        if(str[i] =='')
+          {
+            count++;
+          }
+          cout<<count
+        
+}
+}
